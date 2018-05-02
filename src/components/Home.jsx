e074938d91c30ab5f63267f9ccd481ee57a785fc
irsyadpahlapi <irsyadpahlapi@gmail.com>
2018-05-02 19:40:52 +0700

    tambah pagination

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,15 +6,37 @@ class Home extends Component {
   constructor() {
     super();
     this.state = {
-      heros: []
+      idprev: 0,
+      idnext: 9,
+      heros: [],
+      heros10: []
     }
   };
   componentDidMount() {
     axios.get('https://api.opendota.com/api/heroes')
     .then ( response => {
       this.setState(prevState => ({
-        heros: response.data
+        heros: response.data,
+        heros10: response.data.slice(0,9)
       }))
+    })
+  }
+  prevs () {
+    let prevpage = this.state.heros10[0]
+    let index = this.state.heros.findIndex( hero => hero.id === prevpage.id)
+    this.setState({
+      heros10: this.state.heros.slice(index-9,index),
+      idprev: index-9,
+      idnext: index
+    })
+  }
+  nexts () {
+    let prevpage = this.state.heros10[0]
+    let index = this.state.heros.findIndex( hero => hero.id === prevpage.id) + 9
+    this.setState({
+      heros10: this.state.heros.slice(index,index + 9),
+      idprev:index,
+      idnext:index+9
     })
   }
   render() {
@@ -23,7 +45,7 @@ class Home extends Component {
         <h3 style={{textAlign:'center'}}>name heros dota 2</h3>
         <div className="showitem" >
           {
-            this.state.heros.map(hero => {
+            this.state.heros10.map(hero => {
               return (
                 <div className="item col-4" key={hero.id}>
                   <img src={`https://api.adorable.io/avatars/191/${hero.localized_name}`} />
@@ -36,6 +58,15 @@ class Home extends Component {
             })
           }
         </div>
+        <div style={{textAlign:'center'}}>
+          {
+            this.state.idprev > 0 ?<button onClick={this.prevs.bind(this)}>prev</button>:""
+          }
+          {
+            this.state.idnext < 110 ?<button onClick={this.nexts.bind(this)}>next</button>:""
+          }
+        </div>
+
       </div>
     );
   }
